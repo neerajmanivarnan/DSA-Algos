b905f5c5c3669a59329aab2ac10fc22418aa901a
Neeraj Manivarnan <neerajmanivarnan666@gmail.com>
2024-10-23 18:07:49 +0530

    heap

--- python/new.py
+++ python/new.py
@@ -1,3 +1,31 @@
-string =  "Neeraj Manivarnan"
-list = ["Neeraj","Manivarnan","is","a","good","boy"]
-print(" ".join(list))
+# Here are the items in the customer's basket. Each item is a tuple
+# of (item name, weight, price per pound).
+#
+basket = [
+("Peaches", 3.0, 2.99),
+("Pears", 5.0, 1.66),
+("Plums", 2.5, 3.99)
+]
+
+
+# Calculate the total price for each item (weight times price per pound)
+# and add them up to get a subtotal.
+#
+subtotal = 0.00
+for item in basket:
+    fruit, weight, unit_price = item
+    subtotal += (weight * unit_price)
+
+
+# Now calculate the sales tax and total bill.
+#
+tax_rate = 0.06625 # 6.625% sales tax in New Jersey
+tax_amt = subtotal * tax_rate
+total = subtotal + tax_amt
+
+
+# Print the receipt for the customer.
+#
+print("Subtotal:  ${:10,.2f}".format(subtotal))
+print("Sales Tax:  ${:10,.2f}".format(tax_amt))
+print("Total:     ${:10,.2f}".format(total))
